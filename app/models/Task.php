f2d9fc5c4e42fc42fe7263e28d847c14f952ba82
Riu <josemanuelriu@gmail.com>
2026-02-02 12:14:50 +0100

    S3 T3 - Create basic structure for view tasks

--- app/models/Task.php
+++ app/models/Task.php
@@ -1,38 +1,30 @@
 <?php
 
-class Task {
+class Task extends Model{
 
-    public function __construct(
-        private int $id,
-        private string $name,
-        private string $description,
-        private State $state,
-        private DateTime $startTime,
-        private ?DateTime $endTime, // "?" allows it to be null if the task has not been completed yet.
-        private DateTime $creation_date,
-        private int $idUser
-    ){}
+    // Path to the JSON file storing tasks
+    protected $jsonFile = ROOT_PATH . '/app/models/tasks.json';
+    
+    public function __construct()
+    {
+        // By leaving this empty, we don't call parent::__construct()
+        // so the app stops looking for a MySQL server.
+    } 
 
-    // Getters: 
+    public function getAllTasks()
+    {
+        if (!file_exists($this->jsonFile)) {
+            return [];
+        }
 
-    public function getId(): int {return $this->id;}
-    public function getName(): string {return $this->name;}
-    public function getDescription(): string {return $this->description;}
-    public function getState(): State {return $this->state;}
-    public function getStartTime(): DateTime {return $this->startTime;}
-    public function getEndTime(): ?DateTime {return $this->endTime;}
-    public function getCreation_date(): DateTime {return $this->creation_date;}
-    public function getIdUser(): int {return $this->idUser;}
+        $jsonContent = file_get_contents($this->jsonFile); //exiting PHP functions -> retrieves a string
+        $data = json_decode($jsonContent, true); // existing PHP function  -> decodes THE string 
+        // true: turns the string into and array  -> $task['name']
 
-    // Setters:
-
-    public function setName(string $name): void {$this->name = $name;}
-    public function setDescription(string $description): void {$this->description = $description;}
-    public function setState(State $state): void {$this->state = $state;}
-    public function setStartTime(DateTime $startTime): void {$this->startTime = $startTime;}
-    public function setEndTime(DateTime $endTime): void {$this->endTime = $endTime;}
-    // All setters except id, creation_date and idUser which are created when instanced and can't be modified. 
-
+        // ** DEBUG: See what PHP actually thinks the data looks like
+        
+        return isset($data['tasks']) ? $data['tasks'] : [];        
+    }
 }
 
 ?>
