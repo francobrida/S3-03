15b923569be2a0f03b8a59469296421215951025
Riu <josemanuelriu@gmail.com>
2026-04-01 16:03:35 +0200

    Deleted comments on tasks files

--- config/routes.php
+++ config/routes.php
@@ -3,11 +3,6 @@
 /**
  * Used to define the routes in the system.
  * 
- * A route should be defined with a key matching the URL and an
- * controller#action-to-call method. E.g.:
- * 
- * '/' => 'index#index',
- * '/calendar' => 'calendar#index'
  */
 $routes = array(
 	'/test' => 'test#index',
@@ -38,15 +33,11 @@ $routes = array(
     '/user/update' => 'user#update',
     '/user/filter' => 'user#filter',
 
-    // Root route:"Landing Page" (Login/Register)
     '/index' => 'user#index',
     
-    // Login: route to authenticate user and create session
     '/login' => 'user#login',
     
-    // Register: route to create a new user as member
     '/register' => 'user#register',
     
-    // Logout: Clear session and redirect to home
     '/logout' => 'user#logout'
 );
